eefd0cf9b2c36424a9f4e66c1e04f5f60713a020
Audrey Agyemang <audreyagyemang310@gmail.com>
2019-05-13 06:30:29 -0400

    add code

---
+++ config/connection.js
@@ -0,0 +1,30 @@
+var mysql = require("mysql");
+
+var connection;
+
+
+if(process.env.JAWSDB_URL)
+{
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+}
+else
+{
+  connection = mysql.createConnection({
+  port: 8080,
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "burgersDB"
+});
+}
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Poblano Picasso Burger');
+INSERT INTO burgers (burger_name) VALUES ('Gourdon-Hamsey Burger');
+INSERT INTO burgers (burger_name) VALUES ('Poutine on the Ritz Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ();
