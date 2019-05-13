e9626a7696f47bb9360f4cf987a0923c5801d577
Audrey Agyemang <audreyagyemang310@gmail.com>
2019-05-13 07:00:02 -0400

    add hmtl/css

---
+++ models/burger.js
@@ -0,0 +1,26 @@
+//code borrowed from Activity 17-CatsApp
+
+var orm = require("../config/orm.js");
+
+var burger = {
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res); //second layer function
+    });
+  },
+
+  // The variables cols and vals are arrays.
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
