32ec23b64acde53f3e570e64b651b1e9261dc888
Audrey Agyemang <audreyagyemang310@gmail.com>
2019-05-18 14:32:38 -0400

    add sql table

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 var mysql = require("mysql");
 
+var connection;
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 
-
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgersDB"
-});
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgersDB"
+  });
+}
 
 
 connection.connect(function(err) {
